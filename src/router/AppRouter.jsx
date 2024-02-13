cfecadc0b1d241254d57d1e366c5b41c6fd734a1
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-12 20:26:46 -0600

    Configuracion de nuestras primeras Rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Routes,Route } from "react-router-dom"
+
+export const AppRouter = () => {
+  return (
+    <Routes> 
+        {/* Login y Registro del Usuario */}
+        <Route />
+
+
+        {/* Persona ya autenticada - panel de Trabajo */}
+        <Route />
+    </Routes>
+  )
+}
