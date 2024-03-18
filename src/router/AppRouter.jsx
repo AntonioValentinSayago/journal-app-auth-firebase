0290263e802ca08d5ad22e34c81778e074425188
AntonioValentin <cesar_anto30@hotmail.com>
2024-03-17 21:04:57 -0600

    Configuracion Global de las Rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,16 @@
 import { Routes,Route } from "react-router-dom"
+import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
 
 export const AppRouter = () => {
   return (
     <Routes> 
         {/* Login y Registro del Usuario */}
-        <Route />
+        <Route path="/auth/*" element={ <AuthRoutes /> }/>
 
 
         {/* Persona ya autenticada - panel de Trabajo */}
-        <Route />
+        <Route path="/*" element={ <JournalRoutes /> } />
     </Routes>
   )
 }
