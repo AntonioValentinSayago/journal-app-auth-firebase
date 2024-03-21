e9ebaa29903b61c3c6706c4e49d56b200a9e8e45
AntonioValentin <cesar_anto30@hotmail.com>
2024-03-20 23:18:50 -0600

    Diseño principal para el NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography variant='h1'>JournalPage</Typography>
       <MailOutline /> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
